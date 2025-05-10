bc3a23f331b3fe71fb0d408dc25943a52683c7ee
Sheriff-Hoti <sherifhoti1235@hotmail.com>
2025-05-10 22:19:50 +0200

    Rm tui command

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
+	"github.com/Sheriff-Hoti/hyprgo/consts"
+	"github.com/Sheriff-Hoti/hyprgo/pkg"
+	"github.com/Sheriff-Hoti/hyprgo/tui"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
@@ -14,12 +19,66 @@ var rootCmd = &cobra.Command{
 
 well well well, the design is inspired by wallrizz".`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		config, config_err := cmd.Flags().GetString("config")
+		dir, dir_err := cmd.Flags().GetString("dir")
+		backend, backend_err := cmd.Flags().GetString("backend")
+		config, config_err := cmd.PersistentFlags().GetString("config")
 
-		if config_err != nil {
-			return config_err
+		if flag_err := errors.Join(dir_err, backend_err, config_err); flag_err != nil {
+			return flag_err
 		}
-		fmt.Println(config)
+
+		if config == "" {
+			config = pkg.GetDefaultConfigPath()
+		}
+
+		//TODO need to change this so the config dir to be configurable
+		kvpairmap, kvpairmap_err := pkg.ReadConfigFile(config)
+
+		if kvpairmap_err != nil {
+			return kvpairmap_err
+		}
+
+		if backend != "" {
+			kvpairmap["backend"] = backend
+		}
+
+		if dir != "" {
+			kvpairmap["wallpaper_dir"] = dir
+		}
+
+		wp_backend := pkg.InitBackend(kvpairmap)
+
+		//TODO make this more dynamic
+		filenames, filenames_error := pkg.GetWallpapers(kvpairmap["wallpaper_dir"])
+
+		if filenames_error != nil {
+			return filenames_error
+		}
+
+		if len(filenames) == 0 {
+			//If no png or jpeg or jpg return error
+			return errors.New("no wallpapers in this directory")
+		}
+
+		RenderImages(filenames)
+
+		fmt.Print("\033[H")
+
+		p := tea.NewProgram(tui.InitialModel(filenames, 0, func(t int) {
+			wp_backend.SetImage(filenames[t])
+			pkg.DataContent(pkg.DataAction{
+				Mode: pkg.Write,
+				Data: &pkg.Data{
+					Current_wallpaper: filenames[t],
+				},
+			})
+
+		}))
+		if _, err := p.Run(); err != nil {
+			fmt.Printf("Alas, there's been an error: %v", err)
+			return err
+		}
+
 		return nil
 	},
 }
@@ -33,4 +92,22 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().StringP("config", "c", "", "specify the config file")
+	rootCmd.Flags().StringP("dir", "d", "", "specify the wallpaper directory")
+	rootCmd.Flags().StringP("backend", "b", "", "specify the wallpaper backend")
+}
+
+func RenderImages(filenames []string) {
+	for idx, filename := range filenames {
+		pkg.IcatCmdHalder(pkg.ICatOptions{
+			Place: pkg.Place{
+				Width:  consts.ICAT_IMAGE_WIDTH,
+				Height: consts.ICAT_IMAGE_HEIGHT,
+				Top:    consts.ICAT_IMAGE_TOP_OFFSET + ((idx / consts.CELL_COLS) * 8),
+				Left:   consts.ICAT_IMAGE_LEFT_OFFSET + ((idx % consts.CELL_COLS) * (consts.ICAT_IMAGE_WIDTH + 3)),
+			},
+			Extra_args:     []string{"--z-index=--1"},
+			Scale_up:       true,
+			Wallpaper_path: filename,
+		})
+	}
 }
