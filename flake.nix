d017a966a8b216acdb4b5162565bb566737cd3db
Sheriff-Hoti <sherifhoti1235@hotmail.com>
2025-05-25 05:07:12 +0200

    Upgrade to nixos 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "TUI Wallpaper Selector";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs =
